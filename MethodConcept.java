e604dc3f97469c4a96625927cc615e4ba736b1eb
PriyankaNakate <nakatepriyanka884@gmail.com>
2024-04-05 22:11:58 +0530

    new pattern program

---
+++ MethodConcept.java
@@ -0,0 +1,49 @@
+class MethodConcept
+{
+	/*public static void main(String[] args) {
+		byte a = 10;
+		// byte b = 20;
+		// add(a,b);
+		add(a,20);
+	}
+	// public static void add(byte num1,byte num2)
+	// {
+	// 	int op = num1+num2;
+	// 	System.out.println(op);
+	// }
+
+	public static void add(byte num1, int num2){
+		System.out.println("byte, int");
+	}
+	public static void add(byte num1, int num2){
+		System.out.println("byte, int");
+	}*/
+
+	/*public static void main(String[] args) {
+		byte a = 10;
+		byte b = 20;
+		add(b+0,a);
+	}
+	public static void add(byte num1,int num2){
+		System.out.println("byte, int");
+	}
+	public static void add(int num1,int num2){
+		System.out.println("int, int");
+	}*/
+
+	public static void main(String[] args) {
+		demo('A');
+	}
+	public static void demo(byte a){
+		System.out.println("byte");
+	}
+	public static void demo(short a){
+		System.out.println("short");
+	}
+	public static void demo(int a){
+		System.out.println("int");
+	}
+	public static void demo(char a){
+		System.out.println("char");
+	}
+}
